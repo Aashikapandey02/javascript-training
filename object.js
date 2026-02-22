1f358de645e9d0aab5ff3a14132ef579b3a09da4
Aashikapandey02 <paashika78@gmail.com>
2026-02-22 09:52:58 +0545

    first commit

---
+++ object.js
@@ -0,0 +1,13 @@
+const person={
+    name:"Aashika",
+    class:12,
+    age:20,
+    skills:["singing","dancing","coding"]
+}
+person.skills.forEach((skill)=>{
+
+})
+// console.log(person.name)
+// console.log(data.class)
+
+console.log(person.skills)
